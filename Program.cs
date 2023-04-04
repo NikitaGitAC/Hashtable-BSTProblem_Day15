2479fa9bd7599671690235a3f03814950fddaf59
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-04 15:03:32 +0530

    inserting value in a tree

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Hash Table and Bst problem");
+            binarytree<int> binarySearch = new binarytree<int>(56);
+            binarySearch.Add(30);
+            binarySearch.Add(70);
+            binarySearch.Display();
         }
     }
 }
